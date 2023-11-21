616ee9ea2c8969f9708387aea650fa6199eb7c4d
victoravalo <victorarvalo@yahoo.com>
2023-11-21 16:03:27 -0500

    Se realiza la obtención de registros de ExperienceData y ModalityTraining ok

---
+++ BusinessLayout/Business/ModalityTraining/ModalityTrainingBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelsDataLayout = DataLayout.Models;
+using ModelsBusinessLayout = BusinessLayout.Models;
+using modality = DataLayout.ModalityTraining;
+
+namespace BusinessLayout.Business.ModalityTraining
+{
+    public class ModalityTrainingBL
+    {
+        public List<ModelsBusinessLayout.ModalityTrainingBL> GetAllModalityTraining()
+        {
+            modality.ModalityTrainingDB modality = new modality.ModalityTrainingDB();
+            List<ModelsDataLayout.ModalityTraining> data = modality.GetAllModalityTrainig();
+            try
+            {
+                return Mapper.ModalityTrainingMapper.MTDLtoMTBL(data);
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ webapi/Controllers/ModalityTraining.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBusiness = BusinessLayout.Models;
+using Business = BusinessLayout.Business;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModalityTraining : ControllerBase
+    {
+        [HttpGet]
+        public List<ModelBusiness.ModalityTrainingBL> GetAllModalityTraining()
+        {
+            Business.ModalityTraining.ModalityTrainingBL modalityTraining = new Business.ModalityTraining.ModalityTrainingBL();
+            return modalityTraining.GetAllModalityTraining();
+        }
+
+    }
+}
