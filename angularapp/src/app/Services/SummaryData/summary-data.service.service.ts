e3f481a49db34c16a071c38d34ec4e29b0fe5399
victoravalo <victorarvalo@yahoo.com>
2023-11-30 16:30:17 -0500

    Se realiza la presentación de objetivos personales y profesionales

--- angularapp/src/app/Services/SummaryData/summary-data.service.service.ts
+++ angularapp/src/app/Services/SummaryData/summary-data.service.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SummaryDataServiceService {
 
-  constructor() { }
+  private myAppUrl = "https://localhost:7211/"
+  private myApiUrl = "api/SummaryData"
+  constructor(private http: HttpClient) { }
+
+  getListPersonalData():Observable<any>{
+    return this.http.get(this.myAppUrl + this.myApiUrl);
+  }
 }
